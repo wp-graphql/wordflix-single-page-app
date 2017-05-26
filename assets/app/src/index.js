d889368ff98ac2ba524e57ce4a27e30156db3fb3
jasonbahl <jasonbahl@mac.com>
2017-05-26 15:20:51 -0600

    step-2-prepare-react-app
    In this step, we’re removing the boilerplate that came with Create React App so we can start working on our app.
    
    Here, we remove all the files in the /app/src directory except for the index.js file, which is the main entry point to our app.
    
    And we also modify the index.js file to contain 1 React component, the “App” component, which renders a <div> with an <h1> saying: “WordFlix, your friendly super powered single page app!”
    
    If you run “npm run start” you should see a very minimal app with just your new H1 being rendered.
    
    In the next step, we’ll build the static assets to replace what was built before, and will properly update the WordPress theme to use this new minimal version of our App.

--- assets/app/src/index.js
+++ assets/app/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+
+/**
+ * This is the only component we actually render directly to the DOM!
+ */
+class App extends Component {
+  render() {
+    return(
+      <h1>WordFlix, your friendly super powered single page app!</h1>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
