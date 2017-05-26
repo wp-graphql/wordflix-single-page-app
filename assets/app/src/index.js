574275aeaaf953fa6711116dc8bd7084867b9899
jasonbahl <jasonbahl@mac.com>
2017-05-26 16:45:23 -0600

    step-3-home-page-styled-components
    In this step, we’re creating a separate Homepage component and passing it to our App component.
    
    This is an example of “composability” in React, where you can create components that are composed of multiple components.
    
    In the next step, we’ll look at how we can add some style to the components!

--- assets/app/src/index.js
+++ assets/app/src/index.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+class HomePage extends Component {
+  render() {
+    return(
+      <div>
+        <h1>WordFlix</h1>
+        <h2>Your favorite super-powered Single Page App for browsing Super Hero Movies!</h2>
+        <a href="#0">Explore Films</a>
+      </div>
+    )
+  }
+}
+
 /**
  * This is the only component we actually render directly to the DOM!
  */
 class App extends Component {
   render() {
     return(
-      <h1>WordFlix, your friendly super powered single page app!</h1>
+      <HomePage />
     )
   }
 }
