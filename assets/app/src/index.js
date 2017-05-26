1c022bd29c564ecee43dcb33af14a550602b9c3a
jasonbahl <jasonbahl@mac.com>
2017-05-26 17:12:19 -0600

    step-3-home-page-styled-components
    In this step, we created a few more styled components to use on our homepage.
    
    You’ll also notice, we imported the “Link” component from ‘react-router’ to use for our button to link to the “/movies” page…I guess that means we should make a Movies page and learn about routing?
    
    First, I think our index file is getting a little big. Before we move to routing, lets put the homepage in it’s own file.

--- assets/app/src/index.js
+++ assets/app/src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+import { Link } from 'react-router';
 
 /**
  * NOTE: This imports the entire Ant Design CSS, which is overkill. . .I would recommend only importing the parts
@@ -28,13 +29,56 @@ const Home = styled.div`
   }
 `;
 
+/**
+ * Component that styles the homepage title
+ */
+const HomeTitle = styled.h1`
+  font-family: avengeance_mightiest_avengeRg;
+  font-size: 72px;
+  line-height: 90px;
+`;
+
+/**
+ * Component that styles the homepage description
+ */
+const Description = styled.div`
+  font-family: TTFirsMedium;
+  font-size: 45px;
+  line-height: 50px;
+  padding: 30px;
+  max-width:750px;
+  text-align:center;
+  margin-bottom:40px;
+`;
+
+/**
+ * Component that styles the "Explore" button
+ */
+const ExploreButton = styled.span`
+  background:#ad0000;
+  color:white;
+  border-radius:25px;
+  text-align:center;
+  font-size: 25px;
+  line-height: 40px;
+  padding: 10px 20px;
+  font-family:TTFirsMedium;
+  &:hover{
+    background:#0087be;
+  }
+`;
+
 class HomePage extends Component {
   render() {
     return(
       <Home>
-        <h1>WordFlix</h1>
-        <h2>Your favorite super-powered Single Page App for browsing Super Hero Movies!</h2>
-        <a href="#0">Explore Films</a>
+        <section>
+          <HomeTitle>WordFlix</HomeTitle>
+          <Description>Your favorite super-powered Single Page App for browsing Super Hero Movies!</Description>
+          <Link to="/movies">
+            <ExploreButton>Explore Films</ExploreButton>
+          </Link>
+        </section>
       </Home>
     )
   }
